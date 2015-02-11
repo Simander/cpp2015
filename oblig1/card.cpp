0d5aeb825efdb4c2705e987c9edda3c443ff1f29
Anders Simonsen <anders.stende.simonsen@gmail.com>
2015-02-11 02:13:44 +0100

    Create card.cpp

---
+++ oblig1/card.cpp
@@ -0,0 +1,57 @@
+#include "card.hpp"
+#include <string>
+using namespace std;
+
+	//Constructor
+	Card::Card(int s, int v){
+		_suit = s;
+		_value = v;
+	}
+	
+	//returns value of card
+	int Card::getValue(){
+		return _value;
+	}
+
+	//returns suit of card
+	int Card::getSuit(){
+		return _suit;
+	}
+	
+	//toString card
+	string Card::toString(){
+		string s;
+		switch(_value){
+			case 1:
+				s+="ace of ";
+				break;
+			case 11:
+				s+="knight of ";
+				break;
+			case 12:
+				s+="queen of ";
+				break;
+			case 13:
+				s+="king of ";
+				break;
+			default:
+				
+				s+= to_string(_value) +" of ";
+				break;
+		}
+		switch(_suit){
+			case 0:
+				s+= /*"diamond";*/"♦";
+				break;
+			case 1:
+				s+= /*"hearts";*/"♥";
+				break;
+			case 2:
+				s+=/*"spades";*/"♠";
+				break;
+			case 3:
+				s+= /*"clubs";*/"♣";
+				break;
+		}
+		return s;
+	}
