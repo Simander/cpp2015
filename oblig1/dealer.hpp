0dfb242b7a1b90cb2d516e77f9c13cf5e599308f
Anders Simonsen <anders.stende.simonsen@gmail.com>
2015-03-05 22:20:16 +0100

    fixed

--- oblig1/dealer.hpp
+++ oblig1/dealer.hpp
@@ -4,24 +4,25 @@
 #include "card.hpp"
 #include "deck.hpp"
 #include <vector>
-//#include<string>
+
 class Dealer{
 
-	private: Deck deck;
-
-	vector<Card> hand;
-	
+	private: 
+		Deck deck;			//the deck of cards from which the dealer deals.
+		vector<Card> hand;	//The dealers hand, holds his cards pr. round.
+		int _numberOfCards;	//numberOfCards in hand
+		int _points;		//counts points during a round
+		
 	public:
-	int numberOfCards;
-	int _points;
-	bool stand;
+	
 
-	Dealer();
-	void shuffle();
-	Card deal();
-	bool recieveCard();
-	int getSum();
-	void resetStats();
-	int getNumberOfCards();
+	Dealer();			//Constructor
+	void shuffle();		//Method for shuffling the deck
+	Card deal();		//Method for dealing a single card from the deck
+	bool recieveCard();	//Method for recieving a card from the deck.
+	int getSum();		//Method that returns the sum of points during a round.
+	void resetStats();	//Method that resets the dealers stats, and shuffles the deck
+	int getNumberOfCards();	//Method returns number of cards in the dealers hand.
 
 };
+#endif
