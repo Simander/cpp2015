d48e9ff962209cded73361a1756ac9edce20ed43
Anders Simonsen <anders.stende.simonsen@gmail.com>
2015-03-05 22:23:43 +0100

    fixed

--- oblig1/player.cpp
+++ oblig1/player.cpp
@@ -1,19 +1,26 @@
 #include "player.hpp"
 #include <string>
 #include <iostream>
+
 using namespace std;
+
 Player::Player(){
 	_numberOfCards = 0;
 	_points = 0;
 	_wallet = 1000;
 }
+
+//updates the players wallet according to result of previous round.
 void Player::winlose(int i){
 	_wallet+= i;
 }
 
+//returns the players points pr. current round
 int Player::getSum(){
 	return _points;
 }
+
+//resets the players points, and removes the cards from the players hand
 void Player::resetStats(){
 	_points = 0;
 	_numberOfCards = 0;
@@ -21,14 +28,22 @@ void Player::resetStats(){
 		hand.pop_back();
 }
 
+//returns the players _wallet
 int Player::getWallet(){
 	return _wallet;
 }
 
+//Puts a card in the players hand
 bool Player::recieveCard(Card a){
 	hand.push_back(a);
 	_numberOfCards++;
-	if(a.getValue() < 10)
+	if(a.getValue() == 1){
+		if(_points+11 <= 21)
+			_points += 11;
+		else
+			_points += 1;
+	}
+	else if(a.getValue() < 10)
 		_points+= a.getValue();
 	else
 		_points+=10;
@@ -38,14 +53,18 @@ bool Player::recieveCard(Card a){
 	else
 		return true;
 }
+
+//returns a card from players hand by index
 Card Player::getCard(int i){
 	return hand.at(i);
 }
 
+//returns the number of cards in the players hand.
 int Player::getNumberOfCards(){
 	return _numberOfCards;
 }
 
+//toString method, returns The cards in the players hand and how many points.
 string Player::toString(){
 	string s ="Your hand: ";
 	for(int i = 0; i < _numberOfCards; i++){
