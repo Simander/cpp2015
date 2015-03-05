b21162e9d72065fd8187b64aa5a646e5eff15b9e
Anders Simonsen <anders.stende.simonsen@gmail.com>
2015-03-05 22:18:15 +0100

    fixed

--- oblig1/card.hpp
+++ oblig1/card.hpp
@@ -12,9 +12,10 @@ class Card{
 		int getValue();			//returns the cards value
 		int getSuit();			//returns the cards suit
 		std::string toString();		//returns a string with all the card data;
-		//ostringstream oss;
+		
 	private:
 		int _suit;	//0-diamonds, 1-hearts, 2-spades, 3-clubs
 		int _value;	//ace 1, knight 10, queen 11, king 12 else, numbers 2-9
 	};
-	#endif
+
+#endif
