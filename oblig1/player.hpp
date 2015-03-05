9e9c6a4d65645f434d120bf9bc051ceff1d789e0
Anders Simonsen <anders.stende.simonsen@gmail.com>
2015-03-05 22:24:22 +0100

    fixed

--- oblig1/player.hpp
+++ oblig1/player.hpp
@@ -4,24 +4,25 @@
 #include "card.hpp"
 #include <vector>
 #include <string>
-//using namespace std;
 
 class Player{
-	int _numberOfCards;
-	int _wallet;
+	private:
+		int _numberOfCards;		//keeps track of how many cards is in the players hand.
+		int _wallet;			//The player wallet
+		int _points;			//How many points the player has pr. round
+		std::vector<Card> hand;	//The players hand.
 
 	public: 
-		int _points;
-		bool stand;
-		std::vector<Card> hand;
-		Player();	
-		bool recieveCard(Card a);
-		int getNumberOfCards();
-		std::string toString();
-		int getSum();
-		void resetStats();
-		void winlose(int i);
-		int getWallet();
-		Card getCard(int i);
+
+		Player();					//Constructor
+		bool recieveCard(Card a);	//method that puts a card in the players hand
+		int getNumberOfCards();		//return number of cards in the players hand
+		std::string toString();		//toString method returns cards and points.
+		int getSum();				//returns the players points for the current round.
+		void resetStats();			//resets the players score and removes cards from hand.
+		void winlose(int i);		//updates wallet according to result of previous round.
+		int getWallet();			//return wallet
+		Card getCard(int i);		//return a card by index.
 };
+
 #endif
